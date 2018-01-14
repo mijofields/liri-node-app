e51b3e2cc305e5c9fde00e37ed68ea99a47fea76
El Pirata Mike <mikefields81@gmail.com>
2018-01-14 11:30:44 -0600

    all files linked and keys updated

--- liri.js
+++ liri.js
@@ -1,2 +1,9 @@
-require("dotenv").config();
+require("dotenv").config();  //detenv configure
+var Twitter = require('twitter'); //twitter API
+var Spotify = require('node-spotify-api'); // Spotify API
+var request = require('request'); // request
+var keys = require("./keys.js") //keys info
 
+
+
+//http://www.omdbapi.com/?i=tt3896198&apikey=5ada800f /OMDB key
